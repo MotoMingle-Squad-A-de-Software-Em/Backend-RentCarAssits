b1f275888748931c185ba9777b3cc69787e7e45b
Leonardo Aquino <u20201b949@upc.edu.pe>
2024-05-28 22:49:04 -0500

    Initial project

---
+++ src/main/java/com/motomingle/rentcarassits/RentcarassitsApplication.java
@@ -0,0 +1,13 @@
+package com.motomingle.rentcarassits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentcarassitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentcarassitsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/motomingle/rentcarassits/RentcarassitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motomingle.rentcarassits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentcarassitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
