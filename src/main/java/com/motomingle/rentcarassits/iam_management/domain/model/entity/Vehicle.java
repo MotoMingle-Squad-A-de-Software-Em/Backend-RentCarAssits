a0b54af56421006f1c6939f02dcc6fdce55fc74c
Leonardo Cesías Díaz <lpcd_04@hotmail.com>
2024-06-16 11:52:27 -0500

    Created Vehicle entity

---
+++ src/main/java/com/motomingle/rentcarassits/iam_management/domain/model/entity/Vehicle.java
@@ -0,0 +1,60 @@
+package com.motomingle.rentcarassits.iam_management.domain.model.entity;
+
+import com.motomingle.rentcarassits.subscription_management.domain.model.entity.Subscription;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "vehicles")
+public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @NotNull
+    @Column(name = "model")
+    private String model;
+
+    @NotBlank
+    @NotNull
+    @Column(name = "brand")
+    private String brand;
+
+    @NotBlank
+    @NotNull
+    @Column(name = "address")
+    private String address;
+
+    @NotBlank
+    @NotNull
+    @Column(name = "seats")
+    private int seats;
+
+    @NotBlank
+    @NotNull
+    @Column(name = "stars")
+    private float stars;
+
+    @NotBlank
+    @NotNull
+    @Column(name = "features")
+    private String features;
+
+    @NotBlank
+    @NotNull
+    @Column(name = "price")
+    private float price;
+
+    @NotBlank
+    @NotNull
+    @Column(name = "vehicle_picture")
+    private String vehiclePicture;
+}
