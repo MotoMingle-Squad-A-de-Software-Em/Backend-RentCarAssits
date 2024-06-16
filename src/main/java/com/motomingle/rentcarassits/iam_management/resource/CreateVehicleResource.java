8fcd0153925d6bb09ee95e801344a3622f3725bb
Leonardo Cesías Díaz <lpcd_04@hotmail.com>
2024-06-16 11:53:21 -0500

    Added CreateVehicleResource

---
+++ src/main/java/com/motomingle/rentcarassits/iam_management/resource/CreateVehicleResource.java
@@ -0,0 +1,44 @@
+package com.motomingle.rentcarassits.iam_management.resource;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateVehicleResource {
+    @NotBlank
+    @NotNull
+    private String model;
+
+    @NotBlank
+    @NotNull
+    private String brand;
+
+    @NotBlank
+    @NotNull
+    private String address;
+
+    @NotBlank
+    @NotNull
+    private int seats;
+
+    @NotBlank
+    @NotNull
+    private float stars;
+
+    @NotBlank
+    @NotNull
+    private String features;
+
+    @NotBlank
+    @NotNull
+    private float price;
+
+    @NotBlank
+    @NotNull
+    private String vehiclePicture;
+}
