5bcbec479589474a69fa5b3e5f33dcee2a8795e5
Leonardo Cesías Díaz <lpcd_04@hotmail.com>
2024-06-21 13:57:40 -0500

    Added Update Booking Resource

---
+++ src/main/java/com/motomingle/rentcarassits/iam_management/resource/UpdateBookingResource.java
@@ -0,0 +1,26 @@
+package com.motomingle.rentcarassits.iam_management.resource;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateBookingResource {
+
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    private Long vehicleId;
+
+    @NotNull
+    private LocalDate bookingDate;
+
+    @NotNull
+    private LocalDate returnDate;
+}
